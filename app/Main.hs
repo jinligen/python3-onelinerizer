14db0e17a8f1aeaa83347ca9906b96faa82b013b
sgalal <chromezh@outlook.com>
2019-10-01 16:52:57 +0800

    Parse file

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
+{-# LANGUAGE BlockArguments #-}
+
 module Main where
 
-import Lib
+import System.IO (IOMode(ReadMode), hGetContents, hSetEncoding, utf8, withFile)
+
+import qualified Language.Python.Common as Python
+import qualified Language.Python.Version3 as Python3
 
 main :: IO ()
-main = someFunc
+main = do
+  let srcFileName = "test.py"
+  withFile srcFileName ReadMode \h -> do
+    hSetEncoding h utf8
+    contents <- hGetContents h
+    case Python3.parseModule contents srcFileName of
+      Left e -> pure ()
+      Right (a, _) -> print a
+
+onelinerize :: Python.ModuleSpan -> Bool
+onelinerize = const True
